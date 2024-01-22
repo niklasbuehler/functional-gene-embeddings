1cfcebbadb0197a7738414fdfa62cf7426eb6b9d
Mo Basmaji <extern.mohamad.basmaji@vwfs.io>
2024-01-22 11:28:55 +0100

    create autoencoder modul for general use

---
+++ src/models/autoencoder.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_size, encoding_size, n_hidden):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_size, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, encoding_size),
+            nn.ReLU()   
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(encoding_size, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, input_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
