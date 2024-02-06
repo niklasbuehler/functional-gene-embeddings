a1572d8d09761323b354c69a6883ec47c4969c44
Mo Basmaji <extern.mohamad.basmaji@vwfs.io>
2024-02-06 16:42:43 +0100

    a working version of autoencoder for Gtex

--- notebooks/GTEx/src/Autoencoder.py
+++ notebooks/GTEx/src/Autoencoder.py
@@ -5,15 +5,15 @@ class Autoencoder(nn.Module):
     def __init__(self, input_size, encoding_size, n_hidden=512):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(input_size, encoding_size),
+            nn.Linear(input_size, n_hidden),
             nn.ReLU(),
-            #nn.Linear(n_hidden, encoding_size),
-            #nn.ReLU()   
+            nn.Linear(n_hidden, encoding_size),
+            nn.ReLU()   
         )
         self.decoder = nn.Sequential(
-            #nn.Linear(encoding_size, n_hidden),
-            #nn.ReLU(),
-            nn.Linear(encoding_size, input_size),
+            nn.Linear(encoding_size, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, input_size),
             nn.ReLU()
         )
 
