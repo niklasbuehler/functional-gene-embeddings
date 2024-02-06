b10b6c314eee5d3630c75b9a2abc84f942760500
Mo Basmaji <extern.mohamad.basmaji@vwfs.io>
2024-02-06 11:33:32 +0100

    refactor GTEx autoencoder

---
+++ notebooks/GTEx/src/Autoencoder.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_size, encoding_size, n_hidden=512):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_size, encoding_size),
+            nn.ReLU(),
+            #nn.Linear(n_hidden, encoding_size),
+            #nn.ReLU()   
+        )
+        self.decoder = nn.Sequential(
+            #nn.Linear(encoding_size, n_hidden),
+            #nn.ReLU(),
+            nn.Linear(encoding_size, input_size),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
--- notebooks/GTEx/src/early_stopping.py
+++ notebooks/GTEx/src/early_stopping.py
@@ -1,45 +1,17 @@
-import copy
+import numpy as np
+class EarlyStopping:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
 
-class EarlyStopping():
-  def __init__(self, patience=8, min_delta=-0.1, restore_best_weights=False, restore_app_weights=True):
-    self.patience = patience
-    self.min_delta = min_delta
-    self.restore_best_weights = restore_best_weights
-    self.restore_app_weights = restore_app_weights
-    self.best_model = None
-    self.app_model = None #the model that may perfrom a bit worse than the best on the test data but better on train data
-    self.best_r2 = None
-    self.app_r2 = None
-    self.counter = 0
-    self.status = ""
-
-  def __call__(self, model, test_r2):
-    if self.best_r2 == None:
-      self.best_r2 = test_r2
-      self.app_r2 = test_r2
-      self.best_model = copy.deepcopy(model)
-      self.app_model = copy.deepcopy(model)
-
-    elif test_r2 - self.best_r2 >= 0:
-      self.best_r2 = test_r2
-      self.app_r2 = test_r2
-      self.counter = 0
-      self.best_model.load_state_dict(model.state_dict())
-      self.app_model.load_state_dict(model.state_dict())
-
-    elif test_r2 - self.best_r2 >= self.min_delta:
-      self.counter = 0
-      self.app_r2 = test_r2
-      self.app_model.load_state_dict(model.state_dict())
-
-    elif test_r2 - self.best_r2 < self.min_delta:
-      self.counter += 1
-      if self.counter >= self.patience:
-        self.status = f"Stopped on {self.counter}"
-        if self.restore_app_weights:
-          model.load_state_dict(self.app_model.state_dict())
-        elif self.restore_best_weights:
-          model.load_state_dict(self.best_model.state_dict())
-        return True
-    self.status = f"{self.counter}/{self.patience}"
-    return False
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
